8d209e2a436318e0b7402d98cf20433633f0807b
Иван Сафронов <safroniv@mail.ru>
2022-07-18 00:11:56 +0300

    Добавьте файлы проекта.

---
+++ JobLessonAlgo08v01Part01/Program.cs
@@ -0,0 +1,8 @@
+﻿//1. Bucketsort
+//Реализовать Bucketsort, проверить корректность работы.
+//2. External sort (доп задание)
+//Реализовать External sort, проверить корректность работы.
+//Реализовать можно на основе Bucketsort,
+//но не обязательно.
+
+Console.WriteLine("Hello, World!");
