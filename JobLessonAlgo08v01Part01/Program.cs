475f5e9cb5475fa20cd904e080c47d31cbc908e5
Иван Сафронов <safroniv@mail.ru>
2022-07-18 21:10:59 +0300

    GB - Алгоритмы. Урок 8 Задание 1. готово только Bucket.

--- JobLessonAlgo08v01Part01/Program.cs
+++ JobLessonAlgo08v01Part01/Program.cs
@@ -5,5 +5,21 @@
 //Реализовать можно на основе Bucketsort,
 //но не обязательно.
 
+Console.WriteLine("Введите размер сортируемого массива");
+int NumInts = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+int[] arrayToSort = new int[NumInts];
+Random random = new Random();
+
+for (int i = 0; i < arrayToSort.Length; i++)
+{
+    arrayToSort[i] = (int)random.NextInt64(0, 10000);
+}
+
+Console.WriteLine($"Несортированный массив: \n {string.Join(" , ", arrayToSort)}");
+var sortedList = BucketSortMethod.BucketSort(arrayToSort, 3);
+
+Console.WriteLine($"Отсортированный массива: \n {string.Join(" , ", sortedList)}");
+
+
+
